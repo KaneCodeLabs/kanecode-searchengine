e30754639cf4a17593ee2a4ae8485b11c329ded5
Xavi <101738194+xavi-burgos99@users.noreply.github.com>
2022-08-16 17:06:04 +0200

    Search engine browser

--- docs/assets/js/searchengine.js
+++ docs/assets/js/searchengine.js
@@ -29,15 +29,30 @@ var KCSearchEngine = class KCSearchEngine {
 				icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z"/></svg>',
 			},
 			data: [],
-			onBlur: (search, result) => {},
-			onChange: (search, result) => {},
-			onFocus: (search, result) => {},
-			onInput: (search, result) => {},
-			onSearch: (search, result) => {},
-			onSubmit: (search, result) => {},
+			engine: {
+				lowercase: true,
+				normalize: true,
+				spaces: true,
+				trim: true
+			},
+			live: true,
+			onBlur: (results, value) => {},
+			onChange: (results, value) => {},
+			onFocus: (results, value) => {},
+			onInput: (results, value) => {},
+			onSearch: (results, value) => {},
+			onSubmit: (results, value) => {},
 			placeholder: 'Search...',
 			recommendations: {
 				enabled: true,
+				format: (title, value) => {
+					const element = document.createElement('div');
+					element.classList.add('kcsearchengine__recommendation');
+					element.innerHTML = '<span class="kcsearchengine__recommendation-title"></span>';
+					element.children[0].innerText = title;
+					element.setAttribute('value', value);
+					return element;
+				},
 				max: 5,
 				noResultsMessage: 'No results found.',
 				showOnFocus: true,
@@ -50,7 +65,11 @@ var KCSearchEngine = class KCSearchEngine {
 
 		// Merge the default options with the user options
 		this.setOptions(options);
+	}
 
+	clear() {
+		this.input.value = '';
+		this.trigger();
 	}
 
 	render() {
@@ -106,6 +125,26 @@ var KCSearchEngine = class KCSearchEngine {
 				if (!Array.isArray(value) && typeof value !== 'function' && typeof value !== 'string')
 					throw new Error(`The value is not valid for '${key}' option.`);
 				break;
+			case 'engine.lowercase':
+				if (typeof value !== 'boolean')
+					throw new Error(`The value is not valid for '${key}' option.`);
+				break;
+			case 'engine.normalize':
+				if (typeof value !== 'boolean')
+					throw new Error(`The value is not valid for '${key}' option.`);
+				break;
+			case 'engine.spaces':
+				if (typeof value !== 'boolean')
+					throw new Error(`The value is not valid for '${key}' option.`);
+				break;
+			case 'engine.trim':
+				if (typeof value !== 'boolean')
+					throw new Error(`The value is not valid for '${key}' option.`);
+				break;
+			case 'live':
+				if (typeof value !== 'boolean')
+					throw new Error(`The value is not valid for '${key}' option.`);
+				break;
 			case 'onBlur':
 			case 'onChange':
 			case 'onFocus':
@@ -121,6 +160,10 @@ var KCSearchEngine = class KCSearchEngine {
 				break;
 			case 'recommendations.enabled':
 				if (typeof value !== 'boolean')
+					throw new Error(`The value is not valid for '${key}' option.`);
+				break;
+			case 'recommendations.format':
+				if (typeof value !== 'string' && typeof value !== 'function')
 					throw new Error(`The value is not valid for '${key}' option.`);
 				break;
 			case 'recommendations.max':
@@ -145,18 +188,100 @@ var KCSearchEngine = class KCSearchEngine {
 
 		// Check if the key is "data"
 		if (key === 'data') {
-			this.data = value;
+			this.options.data = value;
 		} else {
 			// Check if the key contains a dot
 			if (key.indexOf('.') !== -1) {
 				const [parentKey, childKey] = key.split('.');
-				this.#options[parentKey][childKey] = value;
+				this.options[parentKey][childKey] = value;
 			} else {
-				this.#options[key] = value;
+				this.options[key] = value;
 			}
-			// Load the option
-			this.#loadOption(key, value);
 		}
+
+		// Load the option
+		this.#loadOption(key, value);
+	}
+
+	trigger() {
+		if (this.#options.recommendations.enabled)
+			this.#engine();
+
+		console.log(this.values);
+	}
+
+	#processData(data) {
+
+		// Initialize the data array
+		let processedData = [];
+
+		// Get the options data if not provided
+		if (typeof data === 'undefined')
+			data = this.options.data;
+		
+		// Check if the data is an array
+		if (Array.isArray(data)) {
+			
+			// Check if the data is all an array of strings or objects
+			if (data.every(item => typeof item === 'string' || (typeof item === 'object' && (typeof item?.title === 'string' || typeof item?.title === 'number')))) {
+				
+				// Loop through the data and process each item
+				data.forEach(item => {
+
+					// Convert the strings to objects
+					if (typeof item === 'string')
+						processedData.push({ title: item, value: item });
+
+					// Check the object properties and fill the missing ones
+					else {
+						const _item = { title: item.title, value: item.title };
+						if (Array.isArray(item.keywords))
+							if (item.keywords.every(keyword => typeof keyword === 'string' || typeof keyword === 'number'))
+								_item.keywords = item.keywords;
+						if (typeof item.format === 'string' || typeof item.format === 'function')
+							_item.format = item.format;
+						if (typeof item.onclick === 'function')
+							_item.onclick = item.onclick;
+						if (typeof item.url === 'string')
+							_item.url = item.url;
+						if (typeof item.value === 'string' || typeof item.value === 'number')
+							_item.value = item.value;
+
+						processedData.push(_item);
+					}
+				});
+			}
+		}
+
+		// Check if the data is an string
+		else if (typeof data === 'string') {
+			processedData = async function() {
+				return fetch(data)
+					.then(response => response.json())
+					.then(data => { return data })
+					.catch(error => { return [] });
+			}
+
+			// Load data if live search is disabled
+			if (!this.options.live)
+				this.#engine();
+		}
+
+		// Check if the data is a function
+		else if (typeof data === 'function') {
+			processedData = data;
+
+			// Load data if live search is disabled and the data is an async function
+			if (!this.options.live && this.data.constructor.name === 'AsyncFunction')
+				this.#engine();
+		}
+
+		// Throw an error if the data is not valid
+		else
+			new Error(`The value of 'data' option is not valid.`);
+
+		// Return the processed data
+		return processedData;
 	}
 
 	#generate() {
@@ -175,6 +300,170 @@ var KCSearchEngine = class KCSearchEngine {
 
 		this.#input = element.querySelector('.kcsearchengine__input');
 		this.#element = element;
+
+		this.input.addEventListener('input', () => {
+			this.trigger();
+		});
+
+		this.input.addEventListener('keydown', (e) => {
+			if (e.key === 'Enter') {
+				e.preventDefault();
+				if (this.options.onSubmit) {
+					try {
+						this.options.onSubmit(this.results, this.input.value);
+					} catch (error) {
+						throw new Error(error);
+					}
+				}
+			}
+			if (e.key === 'Escape') {
+				e.preventDefault();
+				this.clear();
+			}
+		});
+	}
+
+	filter(text) {
+		if (this.options.engine.lowercase)
+			text = text.toLowerCase();
+		if (this.options.engine.normalize)
+			text = text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+		if (this.options.engine.spaces)
+			text = text.replace(/\s/g, '');
+		if (this.options.engine.trim)
+			text = text.trim();
+		return text;
+	}
+
+	#engine(data) {
+
+		// Get the class data if not provided
+		if (typeof data === 'undefined')
+			data = this.data;
+
+		// Check if the data is an array
+		if (Array.isArray(data)) {
+
+			// Do a copy of the data
+			data = [...data];
+
+			// Initialize the results and values arrays
+			this.#results = [];
+			this.#values = [];
+
+			// Get the input value and filter it
+			const value = this.filter(this.input.value);
+			
+			// Exit if the value is empty
+			if (value.length === 0)
+				return;
+
+			// Loop through the data to find the equal title
+			for (let i = 0; i < data.length; i++) {
+				const title = this.filter(data[i].title);
+				if (title === value) {
+					this.results.push(data.splice(i, 1)[0]);
+					i--;
+				}
+			}
+
+			// Loop through the data to find titles that start with the input value
+			for (let i = 0; i < data.length; i++) {
+				const title = this.filter(data[i].title);
+				if (title.startsWith(value)) {
+					this.results.push(data.splice(i, 1)[0]);
+					i--;
+				}
+			}
+
+			// Loop through the data to find similar titles
+			for (let i = 0; i < data.length; i++) {
+				const title = this.filter(data[i].title);
+				if (title.includes(value)) {
+					this.results.push(data.splice(i, 1)[0]);
+					i--;
+				}
+			}
+
+			// Loop through the data to find keywords that are equal to the input value
+			for (let i = 0; i < data.length; i++) {
+				const keywords = data[i].keywords;
+				if (!Array.isArray(keywords))
+					continue;
+				for (let j = 0; j < keywords.length; j++) {
+					const keyword = this.filter(keywords[j]);
+					if (keyword === value) {
+						this.results.push(data.splice(i, 1)[0]);
+						i--;
+						break;
+					}
+				}
+			}
+
+			// Loop through the data to find keywords that start with the input value
+			for (let i = 0; i < data.length; i++) {
+				const keywords = data[i].keywords;
+				if (!Array.isArray(keywords))
+					continue;
+				for (let j = 0; j < keywords.length; j++) {
+					const keyword = this.filter(keywords[j]);
+					if (keyword.startsWith(value)) {
+						this.results.push(data.splice(i, 1)[0]);
+						i--;
+						break;
+					}
+				}
+			}
+
+			// Loop through the data to find similar keywords
+			for (let i = 0; i < data.length; i++) {
+				const keywords = data[i].keywords;
+				if (!Array.isArray(keywords))
+					continue;
+				for (let j = 0; j < keywords.length; j++) {
+					const keyword = this.filter(keywords[j]);
+					if (keyword.includes(value)) {
+						this.results.push(data.splice(i, 1)[0]);
+						i--;
+						break;
+					}
+				}
+			}
+
+			// Set the values from the results array
+			this.#values = this.results.map(item => item.value);
+		}
+
+		// Check if the data is a function
+		else if (typeof data === 'function') {
+
+			// Check if the data is an async function
+			if (data.constructor.name === 'AsyncFunction') {
+
+				// Check if the data needs to be fetched every time the engine is called
+				if (this.options.live) {
+					data().then(data => {
+						this.#engine(this.#processData(data));
+					});
+				}
+
+				// Check if the data needs to be fetched only once
+				else {
+					data().then(data => {
+						this.setOption('data', this.#processData(data));
+					});
+				}
+			}
+
+			// Check if the data is a regular function
+			else {
+				this.#engine(this.#processData(data()));
+			}
+		}
+
+		else {
+			throw new Error('The data is not valid.');
+		}
 	}
 
 	#loadOption(key, value) {
@@ -197,6 +486,9 @@ var KCSearchEngine = class KCSearchEngine {
 				else
 					this.element.querySelector('.kcsearchengine__button').classList.remove('kcsearchengine__button-icon');
 				break;
+			case 'data':
+				this.#data = this.#processData();
+				break;
 			case 'placeholder':
 				this.input.placeholder = value;
 				break;
@@ -210,12 +502,18 @@ var KCSearchEngine = class KCSearchEngine {
 				this.#themes.forEach(theme => this.element.classList.remove(`kcsearchengine__theme-${theme}`));
 				this.element.classList.add(`kcsearchengine__theme-${value}`);
 				break;
+			case 'engine.lowercase':
+			case 'engine.normalize':
+			case 'engine.spaces':
+			case 'engine.trim':
+			case 'live':
 			case 'onBlur':
 			case 'onChange':
 			case 'onFocus':
 			case 'onInput':
 			case 'onSearch':
 			case 'onSubmit':
+			case 'recommendations.format':
 			case 'recommendations.max':
 			case 'recommendations.noResultsMessage':
 			case 'recommendations.showOnFocus':
@@ -225,17 +523,23 @@ var KCSearchEngine = class KCSearchEngine {
 		}
 	}
 
+	get data() { return this.#data; }
 	get defaults() { return this.#defaults; }
 	get element() { return this.#element; }
 	get input() { return this.#input; }
 	get options() { return this.#options; }
+	get results() { return this.#results; }
 	get target() { return this.#target; }
 	get themes() { return this.#themes; }
+	get values() { return this.#values; }
 
+	#data = [];
 	#defaults = {};
 	#element = null;
 	#input = null;
 	#options = {};
+	#results = [];
 	#target = null;
 	#themes = ['light', 'dark', 'clear'];
+	#values = [];
 }
